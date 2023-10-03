b88e398bfb0e12154ab499939a28249e62175bd1
ManuelFBS <manuelf.borrego@gmail.com>
2023-10-03 14:29:03 -0400

    Actualizacion de main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import * as morgan from 'morgan';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.use(morgan('dev'));
+
   const configService = app.get(ConfigService);
+
+  app.setGlobalPrefix('apitest');
 
   await app.listen(configService.get('PORT'));
 }
